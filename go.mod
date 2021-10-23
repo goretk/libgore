fc9982139f7f1c71424900f439f8e1af4bd5a38a
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2021-10-23 12:52:55 +0100

    Update to latest gore

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/goretk/libgore
 
-go 1.13
+go 1.17
+
+require github.com/goretk/gore v0.10.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/goretk/gore v0.8.18
-	github.com/kr/pretty v0.1.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/mod v0.5.1 // indirect
 )
--- structs.h
+++ structs.h
@@ -2,75 +2,80 @@
 // Use of this source code is governed by the license that
 // can be found in the LICENSE file.
 
-struct compilerVersion{
-        char* name;
-        char* sha;
-        char* timestamp;
+struct compilerVersion
+{
+        char *name;
+        char *sha;
+        char *timestamp;
 };
 
-struct function {
-        char* name;
-        int srcLineLength;
-        int srcLineStart;
-        int srcLineEnd;
+struct function
+{
+        char *name;
         unsigned long long offset;
         unsigned long long end;
-        char* fileName;
-        char* packageName;
+        char *packageName;
 };
 
-struct method {
-        char* receiver;
-        struct function* function;
+struct method
+{
+        char *receiver;
+        struct function *function;
 };
 
-struct package {
-        char* name;
-        char* filepath;
-        struct function** function;
-        struct method** method;
+struct package
+{
+        char *name;
+        char *filepath;
+        struct function **function;
+        struct method **method;
         unsigned long numFuncs;
         unsigned long numMeths;
 };
 
-struct packages {
-        struct package** packages;
+struct packages
+{
+        struct package **packages;
         unsigned long length;
 };
 
-struct method_type {
-    char* name;
-    struct type* gotype;
-    unsigned long long ifaceCallOffset;
-    unsigned long long funcCallOffset;
+struct method_type
+{
+        char *name;
+        struct type *gotype;
+        unsigned long long ifaceCallOffset;
+        unsigned long long funcCallOffset;
 };
 
-struct methods_type {
-    struct method_type** methods;
-    unsigned long length;
+struct methods_type
+{
+        struct method_type **methods;
+        unsigned long length;
 };
 
-struct type {
-    unsigned int kind;
-    char* name;
-    unsigned long long addr;
-    unsigned long long ptrResolved;
-    char* packagePath;
-    struct types* fields;
-    char* fieldName;
-    char* fieldTag;
-    int fieldAnon;
-    struct type* element;
-    int length;
-    int chanDir;
-    struct type* key;
-    struct types* funcArgs;
-    struct types* funcReturns;
-    int isVariadic;
-    struct methods_type* methods;
+struct type
+{
+        unsigned int kind;
+        char *name;
+        unsigned long long addr;
+        unsigned long long ptrResolved;
+        char *packagePath;
+        struct types *fields;
+        char *fieldName;
+        char *fieldTag;
+        int fieldAnon;
+        struct type *element;
+        int length;
+        int chanDir;
+        struct type *key;
+        struct types *funcArgs;
+        struct types *funcReturns;
+        int isVariadic;
+        struct methods_type *methods;
 };
 
-struct types {
-    struct type** types;
-    unsigned long length;
+struct types
+{
+        struct type **types;
+        unsigned long length;
 };
